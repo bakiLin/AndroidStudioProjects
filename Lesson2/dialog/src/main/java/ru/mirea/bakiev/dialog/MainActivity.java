5b8ece82487d627ca91cc70370149ae3c610e849
bakiLin <ilnur_bakiev@inbox.ru>
2024-03-12 15:57:38 +0300

    Lesson2 fix

--- Lesson2/dialog/src/main/java/ru/mirea/bakiev/dialog/MainActivity.java
+++ Lesson2/dialog/src/main/java/ru/mirea/bakiev/dialog/MainActivity.java
@@ -1,12 +1,17 @@
 package ru.mirea.bakiev.dialog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,27 @@ public class MainActivity extends AppCompatActivity {
         MyDialogFragment dialogFragment = new MyDialogFragment();
         dialogFragment.show(getSupportFragmentManager(), "mirea");
     }
+
+    public void onClickTimePicker(View view) {
+        MyTimeDialogFragment timeDialogFragment = new MyTimeDialogFragment();
+        timeDialogFragment.show(getSupportFragmentManager(), "mirea");
+    }
+
+    public void OnClickDatePicker(View view) {
+        MyDateDialogFragment dateDialogFragment = new MyDateDialogFragment();
+        dateDialogFragment.show(getSupportFragmentManager(), "mirea");
+    }
+
+    public void OnProgressDialog(View view) {
+        MyProgressDialogFragment progressDialogFragment = new MyProgressDialogFragment();
+        progressDialogFragment.show(getSupportFragmentManager(), "mirea");
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {}
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {}
 
     public void onOkClicked() {
         Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Иду дальше\"!",
