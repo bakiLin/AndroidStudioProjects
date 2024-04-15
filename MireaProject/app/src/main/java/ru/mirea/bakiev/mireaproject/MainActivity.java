8c31d998082919bece55b828a8fd14047a0cb260
bakiLin <ilnur_bakiev@inbox.ru>
2024-04-15 14:01:48 +0300

    Upload MireaProject

---
+++ MireaProject/app/src/main/java/ru/mirea/bakiev/mireaproject/MainActivity.java
@@ -0,0 +1,23 @@
+package ru.mirea.bakiev.mireaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Fragment fragment;
+    FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragment  = new WorkerFragment();
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
+    }
+}
---
+++ MireaProject/app/src/main/java/ru/mirea/bakiev/mireaproject/WorkerFragment.java
@@ -0,0 +1,39 @@
+package ru.mirea.bakiev.mireaproject;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class WorkerFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        WorkRequest workRequest = new OneTimeWorkRequest.Builder(MyWorker.class).build();
+        WorkManager
+                .getInstance(getActivity())
+                .enqueue(workRequest);
+
+        return inflater.inflate(R.layout.fragment_worker, container, false);
+    }
+}
